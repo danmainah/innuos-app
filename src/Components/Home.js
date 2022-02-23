3d2b8daef24f8c4a9f558e0350cac9a13950cbf0
danmainah <danielmainawaweru@gmail.com>
2022-02-23 18:03:23 +0300

    Add reducer to filter fetched data and test it in Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAlbums } from '../redux/albums';
+import { localAlbums } from '../redux/albums/localAlbum';
 import  undefined_album_cover  from '../images/undefined_album_cover.png';
+import qobuz from '../images/qobuz.png';
  
 const Home = () => {
-    const state = useSelector((state) => state.albums);
+    const state = useSelector((state) => state.local);
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getAlbums());
+        dispatch(localAlbums());
       }, [dispatch]);
   return (
     <div>
@@ -16,6 +17,7 @@ const Home = () => {
         {album.artist}
         <p>{album.album}</p>
         { album.cover !=  null? <img src={album.cover} alt= "album cover" />: <img src={undefined_album_cover} alt= "album cover" />}
+        { album.source === "QOBUZ"? <img src={ qobuz} alt = "From Stream Logo" /> : null}
       </div>
     ))}
   </div>
