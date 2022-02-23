c9696b5e80d390bb0cef3e78af4d89c35034052e
danmainah <danielmainawaweru@gmail.com>
2022-02-23 17:28:22 +0300

    Dispay all albums data

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAlbums } from '../redux/albums';
-
+import  undefined_album_cover  from '../images/undefined_album_cover.png';
+ 
 const Home = () => {
     const state = useSelector((state) => state.albums);
     const dispatch = useDispatch();
@@ -13,6 +14,8 @@ const Home = () => {
     {state.map((album) => (
       <div key={`${album.id}`} >
         {album.artist}
+        <p>{album.album}</p>
+        { album.cover !=  null? <img src={album.cover} alt= "album cover" />: <img src={undefined_album_cover} alt= "album cover" />}
       </div>
     ))}
   </div>
