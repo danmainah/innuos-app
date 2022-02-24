0ed31b7ccf1e68bd088a65b8335d8341512a1492
danmainah <danielmainawaweru@gmail.com>
2022-02-24 22:21:29 +0300

    Add styling for albums component

--- src/Components/Albums.js
+++ src/Components/Albums.js
@@ -12,16 +12,20 @@ const Albums = () => {
         dispatch(getAlbums());
       }, [dispatch]);
   return (
-    <div>
+    <div className="container">
     <Navbar />
+    <div className="row bg-dark">
     {state.map((album) => (
-      <div key={`${album.id}`} >
-        {album.artist}
-        <p>{album.album}</p>
-        { album.cover !=  null? <img src={album.cover} alt= "album cover" />: <img src={undefined_album_cover} alt= "album cover" />}
+      <div key={`${album.id}`} class="col-2 col-md-4 col-sm-12 card cardwidth bg-dark p-2">
+        { album.cover !=  null? <img src={album.cover} alt= "album cover" className="card-img-top imgwidth"/>: <img src={undefined_album_cover} alt= "album cover" />}
+        <div className="card-body">
+          <p className="card-text text-white">{album.album}</p>
+          <p className="card-text text-secondary">{album.artist}</p>
+        </div> 
         { album.source === "QOBUZ"? <img src={ qobuz} alt = "From Stream Logo" /> : null}
       </div>
     ))}
+    </div>
   </div>
   )
 }
