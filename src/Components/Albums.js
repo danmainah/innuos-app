fa1c17268720b5a0b55282537efdf9657a60fbe4
danmainah <danielmainawaweru@gmail.com>
2022-02-26 12:25:01 +0300

    Add stlying to the albums

--- src/Components/Albums.js
+++ src/Components/Albums.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAlbums } from '../redux/albums/albums';
 import  undefined_album_cover  from '../images/undefined_album_cover.png';
 import qobuz from '../images/qobuz.png';
-import Navbar from './Navbar';
+import Navigation from './Navbar';
  
 const Albums = () => {
     const state = useSelector((state) => state.albums);
@@ -13,16 +13,16 @@ const Albums = () => {
       }, [dispatch]);
   return (
     <div className="container">
-    <Navbar />
+    <Navigation />
     <div className="row bg-dark">
     {state.map((album) => (
-      <div key={`${album.id}`} class="col-2 col-md-4 col-sm-12 card cardwidth bg-dark p-2">
-        { album.cover !=  null? <img src={album.cover} alt= "album cover" className="card-img-top imgwidth"/>: <img src={undefined_album_cover} alt= "album cover" />}
+      <div key={`${album.id}`} className="col-2 col-md-4 col-sm-12 card cardwidth bg-dark p-1">
+        { album.cover !=  null? <img src={album.cover} alt= "album cover" className="card-img-top imgwidth"/>: <img src={undefined_album_cover} alt= "album cover" className="imgwidth" />}
+        { album.source === "QOBUZ"? <img src={ qobuz} alt = "From Stream Logo" className="rounded-circle circlesize"/> : null}
         <div className="card-body">
-          <p className="card-text text-white">{album.album}</p>
-          <p className="card-text text-secondary">{album.artist}</p>
+          <p className="card-text text-white textwidth">{album.album}</p>
+          <p className="card-text text-secondary textwidth">{album.artist}</p>
         </div> 
-        { album.source === "QOBUZ"? <img src={ qobuz} alt = "From Stream Logo" /> : null}
       </div>
     ))}
     </div>
--- src/Components/LocalAlbums.js
+++ src/Components/LocalAlbums.js
@@ -12,16 +12,20 @@ const LocalAlbums = () => {
         dispatch(localAlbums());
       }, [dispatch]);
   return (
-    <div>
+  <div className="container">
     <Navigation />
+    <div className="row bg-dark">
     {state.map((album) => (
-      <div key={`${album.id}`} >
-        {album.artist}
-        <p>{album.album}</p>
-        { album.cover !=  null? <img src={album.cover} alt= "album cover" />: <img src={undefined_album_cover} alt= "album cover" />}
-        { album.source === "QOBUZ"? <img src={ qobuz} alt = "From Stream Logo" /> : null}
+      <div key={`${album.id}`} className="col-2 col-md-4 col-sm-12 card cardwidth bg-dark p-1">
+        { album.cover !=  null? <img src={album.cover} alt= "album cover" className="card-img-top imgwidth"/>: <img src={undefined_album_cover} alt= "album cover" className="imgwidth" />}
+        { album.source === "QOBUZ"? <img src={ qobuz} alt = "From Stream Logo" className="rounded-circle circlesize"/> : null}
+        <div className="card-body">
+          <p className="card-text text-white textwidth">{album.album}</p>
+          <p className="card-text text-secondary textwidth">{album.artist}</p>
+        </div> 
       </div>
     ))}
+    </div>
   </div>
   )
 }
